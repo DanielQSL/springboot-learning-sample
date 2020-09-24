e4c606d06c6ad2f4396304e87b3dcce1fff9892e
qianshuailong <qianshuailong@hupu.com>
2020-09-24 18:26:00 +0800

    refactor:修改lua脚本

--- springboot-redis/src/main/resources/scripts/FrequencyControl.lua
+++ springboot-redis/src/main/resources/scripts/FrequencyControl.lua
@@ -2,7 +2,7 @@
 -- 频率控制脚本
 --
 -- 参数：key，过期时间（秒），允许的最大次数，发送时间（时间戳：秒）
--- 返回：0：超出频率；1：未超出频率
+-- 返回：0：未超出频率；1：超出频率
 -- 注意：缓存一周数据，会出现某个时间段断档
 --
 
@@ -14,7 +14,7 @@ if limitLen < tonumber(ARGV[2]) then
     end
 else
     local times = redis.call('LINDEX', KEYS[1], -1)
-    if tonumber(ARGV[3]) - times < tonumber(ARGV[1]) then
+    if tonumber(ARGV[3]) - times <= tonumber(ARGV[1]) then
         return 1
     else
         redis.call('LPUSH', KEYS[1], tonumber(ARGV[3]))
