153504ec9e0325f394487f0bce09deac9f7b2e16
qianshuailong <qianshuailong@hupu.com>
2020-11-11 16:23:09 +0800

    feat:加入JsonIgnoreProperties注释

--- springboot-redis/src/main/java/com/qsl/springboot/dto/StudentDTO.java
+++ springboot-redis/src/main/java/com/qsl/springboot/dto/StudentDTO.java
@@ -1,11 +1,13 @@
 package com.qsl.springboot.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 /**
  * @author qianshuailong
  * @date 2020/7/6
  */
+@JsonIgnoreProperties(ignoreUnknown = true) // 该注释用于反序列化时忽略不认识的字段
 @Data
 public class StudentDTO {
 
